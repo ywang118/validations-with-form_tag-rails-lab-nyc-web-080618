0a801f0e4628c32cd70fa19ada5bc1f0ddada43e
Yixuan Wang <ywang0506@gmail.com>
2018-09-03 16:10:49 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,23 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+
   end
 
   def edit
     @post = Post.find(params[:id])
   end
 
+
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    if @post.update(post_params)
 
-    redirect_to post_path(@post)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end  
   end
 
   private
